95f54323806151a80da59517ca5fa577bb825e91
Mumo Nzinga <mnzinga888@gmail.com>
2019-05-02 04:53:15 +0300

    change DB

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -7,7 +7,7 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     protected void before() {
-        main.java.DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "mumo", "pw*kinyanjui");
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "mumo", "pw*kinyanjui");
     }
 
     @Override
@@ -16,7 +16,7 @@ public class DatabaseRule extends ExternalResource {
 
         String deleteEndangeredAnimalsQuery = "DELETE FROM endangered_animals *;";
         String deleteSightingsQuery = "DELETE FROM sightings *;";
-        try(Connection con = main.java.DB.sql2o.open()) {
+        try(Connection con = DB.sql2o.open()) {
             ((org.sql2o.Connection) con).createQuery(deleteAnimalsQuery).executeUpdate();
             ((org.sql2o.Connection) con).createQuery(deleteEndangeredAnimalsQuery).executeUpdate();
             ((org.sql2o.Connection) con).createQuery(deleteSightingsQuery).executeUpdate();
